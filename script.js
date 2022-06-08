d7de6dfebc050f363bcd27e7d9280099ed5b05e2
Sangeetha <83246904+kadir12386@users.noreply.github.com>
2022-06-08 19:07:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const countDate = new Date("Jun 8, 2022 00:00:00:").getTime();
+console.log(countDate);
+
+function newYear() {
+  const now = new Date().getTime();
+  const gap = countDate - now;
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const d = Math.floor(gap / day);
+  const h = Math.floor((gap % day) / hour);
+  const m = Math.floor((gap % hour) / minute);
+  const s = Math.floor((gap % minute) / second);
+
+  document.getElementById("day").innerHTML = d;
+  document.getElementById("hour").innerHTML = h;
+  document.getElementById("minute").innerHTML = m;
+  document.getElementById("second").innerHTML = s;
+}
+setInterval(() => {
+  newYear();
+}, 1000);
